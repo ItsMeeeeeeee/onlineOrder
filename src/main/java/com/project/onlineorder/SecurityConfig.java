521dd9d39ec8bea405de5476c2d11fbc2c41f1e7
ItsMeeeeeeee <yl1381@georgetown.edu>
2022-05-20 11:54:46 -0400

    l14

--- src/main/java/com/project/onlineorder/SecurityConfig.java
+++ src/main/java/com/project/onlineorder/SecurityConfig.java
@@ -17,26 +17,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select email, password, enabled from customers where email = ?")
+                .authoritiesByUsernameQuery("select email, authorities from authorities where email = ?");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .formLogin()
                 .failureForwardUrl("/login?error=true");
+
         http
                 .authorizeRequests()
                 .antMatchers("/order/*", "/cart", "/checkout").hasAuthority("ROLE_USER")
                 .anyRequest().permitAll();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email, password, enabled FROM customers WHERE email=?")
-                .authoritiesByUsernameQuery("SELECT email, authorities FROM authorities WHERE email=?");
-
     }
 
     @SuppressWarnings("deprecation")
--- src/main/java/com/project/onlineorder/controller/MenuInfoController.java
+++ src/main/java/com/project/onlineorder/controller/MenuInfoController.java
@@ -2,6 +2,8 @@ package com.project.onlineorder.controller;
 
 import com.project.onlineorder.entity.MenuItem;
 import com.project.onlineorder.entity.Restaurant;
+import com.project.onlineorder.service.MenuInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
@@ -10,15 +12,19 @@ import java.util.List;
 
 @Controller
 public class MenuInfoController {
-    @RequestMapping(value = "/restaurant/{restaurantId}/menu", method = RequestMethod.GET)
-    @ResponseBody
-    public List<MenuItem> getMenus(@PathVariable("restaurantId") int restaurantId) {
-        return new ArrayList<>();
-    }
+
+    @Autowired
+    private MenuInfoService menuInfoService;
 
     @RequestMapping(value = "/restaurants", method = RequestMethod.GET)
     @ResponseBody
     public List<Restaurant> getRestaurants() {
-        return new ArrayList<>();
+        return menuInfoService.getRestaurants();
+    }
+
+    @RequestMapping(value = "/restaurant/{restaurantId}/menu", method = RequestMethod.GET)
+    @ResponseBody
+    public List<MenuItem> getMenus(@PathVariable(value = "restaurantId") int restaurantId) {
+        return menuInfoService.getAllMenuItem(restaurantId);
     }
 }
